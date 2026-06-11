928c2269c00fe243dc47e98024ee872af6f3e4ae
juval9340 <juval9340@gmail.com>
2026-06-11 13:25:08 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -11,3 +11,18 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
     Return: העומס P בניוטון (float)
     """
     # כתבו כאן את הקוד
+import numpy as np
+from scipy.optimize import bisect
+
+def f(P):
+    sigma_max = (P / A) * (
+        1 + (e * c / r**2) *
+        (1 / np.cos((L / (2 * r)) * np.sqrt(P / (E * A))))
+    )
+    return sigma_max - sigma_allow
+
+euler_load = (np.pi**2 * E * (A * r**2)) / (L**2)
+
+P = bisect(f, 1e-6, 0.999 * euler_load)
+
+return float(P)
